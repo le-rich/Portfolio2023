c420ab65894f681800298d15b941767c6c368135
le-rich <richardle1997@gmail.com>
2021-05-16 18:46:44 -0700

    Main nav hover negative space highlight

--- src/js/app.js
+++ src/js/app.js
@@ -64,6 +64,12 @@ function init() {
               amount: 0.2
             }
           })
+
+          textrev.eventCallback("onComplete", () =>{
+            document.querySelectorAll('.reveal-line').forEach(element => {
+                element.classList.remove('reveal-line')
+            });
+          });
       },
   
       // Progress
@@ -204,6 +210,48 @@ function init() {
     rayDirection.normalize()
 
     // raycaster.set(rayOrigin, rayDirection)
+    const link = document.querySelectorAll('nav > .hover-this');
+    const cursor = document.querySelector('.cursor');
+
+    const animateit = function (e) {
+      const span = this.querySelector('span');
+      const { offsetX: x, offsetY: y } = e,
+      { offsetWidth: width, offsetHeight: height } = this,
+
+      move = 25,
+      xMove = x / width * (move * 2) - move,
+      yMove = y / height * (move * 2) - move;
+
+      span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+      if (e.type === 'mouseleave'){
+        span.style.transform = '';
+        shrinkCursor();
+      }  else {
+        expandCursor();
+      }
+      
+    };
+
+    const shrinkCursor = function(){
+      cursor.classList.remove('cursor-expand');
+    }
+    const expandCursor = function(){
+        cursor.classList.add('cursor-expand');
+    }
+
+    const editCursor = e => {
+      const {
+        clientX: x,
+        clientY: y
+      } = e;
+      cursor.style.left = x + 'px';
+      cursor.style.top = y + 'px';
+    };
+
+    link.forEach(b => b.addEventListener('mousemove', animateit));
+    link.forEach(b => b.addEventListener('mouseleave', animateit));
+    window.addEventListener('mousemove', editCursor);
 
     /**
      * Sizes
